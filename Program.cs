91d39c9c25f4685b6ac2fd1a31e5df0797d851ae
guinhomoraes <guilhermeguilherme98@gmail.com>
2023-09-22 14:43:37 -0300

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("1561651561616", "Tijolo", "1561561561", 64);
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone = new Iphone("1561651561616", "Iphone XS", "1561561561BGF", 128);
+nokia.InstalarAplicativo("Youtube");
